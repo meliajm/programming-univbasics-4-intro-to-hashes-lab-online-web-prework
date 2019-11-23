bd0f1314c5d9bca352524ee43a30a5a99afe218b
melia miller <meliajm@gmail.com>
2019-11-23 21:07:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,13 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   if hash[key]
-    return hash[key] += 1 
+    hash[key] += 1 
   else
-    return hash[key] = 1
+    hash[key] = 1
 end
+
+# if shipping_manifest["top hat"]
+#   shipping_manifest["top hat"] += 1
+# else
+#   shipping_manifest["top hat"] = 1
+# end
